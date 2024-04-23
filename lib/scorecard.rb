7f2a2fd2c5ef230563b72d90f7d28a1dd8e7e5c4
maybethee <jozalamun@gmail.com>
2024-04-23 17:36:19 -0400

    Add some scoring methods for lower category scores

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -21,6 +21,16 @@ class Scorecard
       yahtzee: nil,
       chance: nil
     }
+
+    @lower_section_methods = {
+      1 => :three_of_a_kind,
+      2 => :four_of_a_kind,
+      3 => :full_house,
+      4 => :small_straight,
+      5 => :large_straight,
+      6 => :yahtzee,
+      7 => :chance
+    }
   end
 
   def add_score(dice)
@@ -81,7 +91,7 @@ class Scorecard
       # perform method that adds code to equivalent upper category code?
       apply_score_upper_category(category_code, dice_arr_numbers)
     else
-      puts "under construction, come back later..."
+      apply_score_lower_category(category_code, dice_arr_numbers)
     end
     display_categories
   end
@@ -99,5 +109,44 @@ class Scorecard
     # perform method that adds code to equivalent upper category code?
     score = dice_array.select { |die| die == upper_section_category }
     @upper_section[@upper_section.keys[(upper_section_category - 1)]] = score.sum
+  end
+
+  def apply_score_lower_category(category_code, dice_array)
+    lower_section_category = category_code[1].to_i
+    puts lower_section_category
+    method = @lower_section_methods[lower_section_category]
+    send(method, dice_array) if method
+  end
+
+  def three_of_a_kind(dice)
+    # use group_by to create hash of dice array grouped by their numbers
+    three_of_a_kind_score = dice.group_by { |die| die }.find { |k, v| v.size >= 3 }
+    three_of_a_kind_score = three_of_a_kind_score ? three_of_a_kind_score.first * 3 : 0
+
+    @lower_section[:three_of_a_kind] = three_of_a_kind_score
+  end
+
+  # same as three of a kind method, but this time... four
+  def four_of_a_kind(dice)
+    four_of_a_kind_score = dice.group_by { |die| die }.find { |k, v| v.size >= 4 }
+    four_of_a_kind_score = four_of_a_kind_score ? four_of_a_kind_score.first * 4 : 0
+
+    @lower_section[:four_of_a_kind] = four_of_a_kind_score
+  end
+
+  def yahtzee(dice)
+    yahtzee = dice.group_by { |die| die }.find { |k, v| v.size >= 5 }
+
+    # yahtzee gives 50 points no matter what die face (except for edge cases, to be added later)
+    @lower_section[:yahtzee] = if yahtzee
+                                 50
+                               else
+                                 0
+                               end
+  end
+
+  def chance(dice)
+    score = dice.sum
+    @lower_section[:chance] = score
   end
 end
